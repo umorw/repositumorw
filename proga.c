93812a52238c33361f1b0a01d80d8f44018a88bd
lucy <lucy@localhost.localdomain>
2016-02-26 20:31:46 +0300

    The program is created. Input of coefficients is added

---
+++ proga.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int x = 0;
+    printf(" a*x^2 + b*x + c = 0\n ");
+    printf("input a =\n ");
+    scanf("%d",&a);
+    printf("input b =\n ");
+    scanf("%d",&b );
+    printf("input c =\n ");
+    scanf("%d",&c);
+    printf("(%d)*x^2 + (%d)*x + (%d) = 0 ",a,b,c);
+    
+    return 0;
+    
+    
+
+
+
+}
