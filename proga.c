415bfdca1b6986fdd98a40e328db5cfc163c9696
lucy <lucy@localhost.localdomain>
2016-02-26 22:11:04 +0300

    Added to a solution of the equation. Style double was a mistake

--- proga.c
+++ proga.c
@@ -3,27 +3,49 @@
 
 int main()
 {
-    double a = 0;
-    double b = 0;
-    double c = 0;
-    double x1 = 0;
-    double x2 = 0;
-	double d = 0;
+    float a = 0;
+    float b = 0;
+    float c = 0;
+
+    float x1 = 0;
+    float x2 = 0;
+
+	float d = 0;
+
     printf(" a*x^2 + b*x + c = 0\n ");
+
     printf("input a =\n ");
-    scanf("%f",&a);
+    	scanf("%f",&a);
+
     printf("input b =\n ");
-    scanf("%f",&b );
+    	scanf("%f",&b );
+
     printf("input c =\n ");
-    scanf("%f",&c);
-    printf("(%f)*x^2 + (%f)*x + (%f) = 0 ",a,b,c);
-    
-    
-    
-    return 0;
-    
-    
+    	scanf("%f",&c);
 
-
+    printf("(%.2f)*x^2 + (%.2f)*x + (%.2f) = 0 ",a,b,c);
+	
+	if( a == 0 ){
+		printf(" the equation is not square\n ");	
+	}
+	else{
+		d = b*b-4*a*c;
+		if(d < 0){
+			printf(" D<0, no decision\n ");
+		}
+		if(d == 0){
+			x1 = (-b)/(2*a);
+			printf(" D=0, one root of equation: %.2f\n ", x1);
+		}
+		if(d > 0){
+			d = sqrt(d);
+			a = 2*a;
+			x1 = (-b-d)/a;
+			x2 = (-b+d)/a;
+			printf(" Equation roots: x1 = %.2f, x2 = %.2f \n ", x1, x2);
+		}
+	}	
+  
+    return 1;
 
 }
