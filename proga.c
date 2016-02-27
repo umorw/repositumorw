e097f6babc1a677a254efa8b93659c801ae32458
lucy <lucy@localhost.localdomain>
2016-02-27 16:17:02 +0300

    Correct

--- proga.c
+++ proga.c
@@ -2,49 +2,50 @@
 #include <math.h>
 
 int main()
+
 {
     float a  = 0;
     float b  = 0;
     float c  = 0;
     float x1 = 0;
     float x2 = 0;
-	float d  = 0;
+    float d  = 0;
 
     printf( " a*x^2 + b*x + c = 0\n " );
 
     printf( " input a =\n " );
-    	scanf( "%f", &a );
+	scanf( "%f", &a );
 
     printf( " input b =\n " );
-    	scanf( "%f", &b );
+	scanf( "%f", &b );
 
     printf( " input c =\n " );
-    	scanf( "%f", &c );
+	scanf( "%f", &c );
 
     printf( "(%.2f)*x^2 + (%.2f)*x + (%.2f) = 0 \n",a,b,c );
-	
-	if( a == 0 ){
-		printf( " the equation is not square\n " );	
+
+    if( a == 0 ){
+	printf( " the equation is not square\n " );	
+    }
+    else{
+	d = b * b - 4 * a * c;
+	if( d < 0 ){
+	    printf(" D < 0, no decision\n ");
 	}
-	else{
-		d = b * b - 4 * a * c;
-		if( d < 0 ){
-			printf(" D < 0, no decision\n ");
-		}
-		if( d == 0 ){
-			x1 = (-b) / ( 2 * a );
-			printf(" D = 0, one root of equation: %.2f\n ", x1);
-		}
-		if( d > 0 ){
-			printf( " D = %.2f\n ", d );
-			float f = sqrt(d);
-			float g = 2 * a;
-			x1 = ( -b - f ) / g;
-			x2 = (-b + f ) / g;
-			printf(" Equation roots: x1 = %.2f, x2 = %.2f \n ", x1, x2);
-		}
-	}	
-  
+	if( d == 0 ){
+	    x1 = (-b) / ( 2 * a );
+	    printf(" D = 0, one root of equation: %.2f\n ", x1);
+	}
+	if( d > 0 ){
+	    printf( " D = %.2f\n ", d );
+	    float f = sqrt(d);
+	    float g = 2 * a;
+	    x1 = ( -b - f ) / g;
+	    x2 = ( -b + f ) / g;
+	    printf(" Equation roots: x1 = %.2f, x2 = %.2f \n ", x1, x2);
+	}
+    }
+
     return 0;
 
 }
